2726d4c69cc43bfd221b1fa5fb3a2c8093584a7f
Dean Thompson <deanthompson.music@gmail.com>
2022-06-09 21:58:57 +0900

    fix: Working on MVVM integration

--- No BS Dictionary/ViewModel/WordViewModel.swift
+++ No BS Dictionary/ViewModel/WordViewModel.swift
@@ -8,6 +8,30 @@
 import Foundation
 
 struct WordViewModel {
-    // add view model!
+    var wordUnits: [WordUnit] = []
+    
+    func fetchSearchedWordsList(forSearchTerm searchTerm: String, completion: @escaping (Result<[WordUnit], NetworkError>) -> Void) {
+        guard let url = URL(string: "https://api.dictionaryapi.dev/api/v2/entries/en/\(searchTerm)") else { return }
+        print(url)
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    completion(.failure(.serverError))
+                    return
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    
+                    let wordUnits = try decoder.decode([WordUnit].self, from: data)
+                    completion(.success(wordUnits))
+                } catch {
+                    completion(.failure(.decodingError))
+                }
+            }
+        }.resume()
+    }
     
 }
